2e633a4ca03e97e1d35ffc335b8bf4eaeab8c313
Putra Nugraha <putnug1122@gmail.com>
2023-07-20 09:45:00 +0800

    neural network done

--- mlp.go
+++ mlp.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math"
+	"os"
 
 	"gonum.org/v1/gonum/mat"
 	"gonum.org/v1/gonum/stat/distuv"
@@ -26,6 +27,30 @@ func CreateNetwork(input, hidden, output int, rate float64) (net Network) {
 	net.hiddenWeights = mat.NewDense(net.hiddens, net.inputs, randomArray(net.inputs*net.hiddens, float64(net.inputs)))
 	net.outputWeights = mat.NewDense(net.outputs, net.hiddens, randomArray(net.hiddens*net.outputs, float64(net.hiddens)))
 	return
+}
+
+func (net Network) Predict(inputData []float64) mat.Matrix {
+	inputs := mat.NewDense(len(inputData), 1, inputData)
+	hiddenInputs := dot(net.hiddenWeights, inputs)
+	hiddenOutputs := apply(sigmoid, hiddenInputs)
+	finalInputs := dot(net.outputWeights, hiddenOutputs)
+	finalOutputs := apply(sigmoid, finalInputs)
+	return finalOutputs
+}
+
+func (net Network) Train(inputData []float64, targetData []float64) {
+	inputs := mat.NewDense(len(inputData), 1, inputData)
+	hiddenInputs := dot(net.hiddenWeights, inputs)
+	hiddenOutputs := apply(sigmoid, hiddenInputs)
+	finalInputs := dot(net.outputWeights, hiddenOutputs)
+	finalOutputs := apply(sigmoid, finalInputs)
+
+	targets := mat.NewDense(len(targetData), 1, targetData)
+	outputErrors := subtract(targets, finalOutputs)
+	hiddenErrors := dot(net.outputWeights.T(), outputErrors)
+
+	net.outputWeights = add(net.outputWeights, scale(net.learningRate, dot(multiply(outputErrors, sigmoidPrime(finalOutputs)), hiddenOutputs.T()))).(*mat.Dense)
+	net.hiddenWeights = add(net.hiddenWeights, scale(net.learningRate, dot(multiply(hiddenErrors, sigmoidPrime(hiddenOutputs)), inputs.T()))).(*mat.Dense)
 }
 
 func dot(m, n mat.Matrix) mat.Matrix {
@@ -93,5 +118,55 @@ func randomArray(size int, v float64) (data []float64) {
 	for i := 0; i < size; i++ {
 		data[i] = dist.Rand()
 	}
+	return
+}
+
+func sigmoid(r, c int, z float64) float64 {
+	return 1.0 / (1 + math.Exp(-1*z))
+}
+
+func sigmoidPrime(m mat.Matrix) mat.Matrix {
+	rows, _ := m.Dims()
+	o := make([]float64, rows)
+	for i := range o {
+		o[i] = 1
+	}
+	ones := mat.NewDense(rows, 1, o)
+	return multiply(m, subtract(ones, m))
+}
+
+func save(net Network) {
+	h, err := os.Create("data/hweights.model")
+	defer h.Close()
+
+	if err == nil {
+		net.hiddenWeights.MarshalBinaryTo(h)
+	}
+
+	o, err := os.Create("data/oweights.model")
+	defer o.Close()
+
+	if err == nil {
+		net.outputWeights.MarshalBinaryTo(o)
+	}
+}
+
+func load(net *Network) {
+	h, err := os.Open("data/hweights.model")
+	defer h.Close()
+
+	if err == nil {
+		net.hiddenWeights.Reset()
+		net.hiddenWeights.UnmarshalBinaryFrom(h)
+	}
+
+	o, err := os.Open("data/oweights.model")
+	defer o.Close()
+
+	if err == nil {
+		net.outputWeights.Reset()
+		net.outputWeights.UnmarshalBinaryFrom(o)
+	}
+
 	return
 }
